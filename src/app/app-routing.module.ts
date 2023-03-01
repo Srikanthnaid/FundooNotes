d01c4759488b12040b327b30725f2d56bd87cd1a
Srikanthnaid <ksru044gmail.com>
2023-03-01 16:29:06 +0530

    ADDED:Routing is added for route from one page to another page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { LoginComponent } from './components/login/login.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:RegistrationComponent},
+  {path:'register',component:RegistrationComponent},
+  {path: 'login',component:LoginComponent},
+  {path: 'forgotpwd',component:ForgotPasswordComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
